d8e14e252881aa541b484a77b497ef0b241d5ed8
mani-hash <manimehalan400@gmail.com>
2024-08-12 17:08:12 +0530

    added main file

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "game.h"
+#include "types.h"
+
+void playGame();
+
+int main()
+{
+    return 0;
+}
+
+void playGame()
+{
+    
+}
