bf0f3da2a9fa3a9451a7174cfb3d2000ebe00fce
mani-hash <manimehalan400@gmail.com>
2024-08-12 17:15:31 +0530

    defined basic macros for game

--- types.h
+++ types.h
@@ -2,5 +2,12 @@
 #define TYPES_H
 
 #include <stdbool.h>
+#define PLAYER_NO 4
+#define PIECE_NO 4
+#define MAX_STANDARD_CELL 52
+#define MAX_HOME_STRAIGHT 20
+#define MAX_COLORS 4
+#define APPROACH_DIFFERENCE 2
+#define MYSTERY_LOCATIONS 6
 
 #endif // !TYPES_H
