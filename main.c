30b016b6898ece321391288b506adfb1ec393a5e
mani-hash <manimehalan400@gmail.com>
2024-08-12 18:59:47 +0530

    print the available players to console

--- main.c
+++ main.c
@@ -30,6 +30,20 @@ void playGame()
 
     struct Player *players = initializePlayers();
 
+    for (int playerIndex = 0; playerIndex < PLAYER_NO; playerIndex++)
+    {
+        printf
+        (
+            "The %s player has %d pieces %s, %s, %s, and %s\n",
+            getName(players[playerIndex].color),
+            PIECE_NO,
+            players[playerIndex].pieces[0].name,
+            players[playerIndex].pieces[1].name,
+            players[playerIndex].pieces[2].name,
+            players[playerIndex].pieces[3].name
+        );
+    }
+    
     // Seed the random number generator
     srand(time(NULL));
 
