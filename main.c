885ea7455c7a7cd6d7d77affb91c37cd01c1f2c6
mani-hash <manimehalan400@gmail.com>
2024-08-12 17:42:56 +0530

    defined the basic structures for the game loop

--- main.c
+++ main.c
@@ -8,10 +8,23 @@ void playGame();
 
 int main()
 {
+    playGame();
     return 0;
 }
 
 void playGame()
 {
-    
+    struct Game game = {
+        0,
+        -1,
+        -1,
+        {[0 ... PLAYER_NO - 1] = -1},
+        {[0 ... PLAYER_NO - 1] = -1},
+        {[0 ... MAX_STANDARD_CELL - 1] = -1},
+
+    };
+
+    struct Piece *standardCells[MAX_STANDARD_CELL][PIECE_NO] = {NULL};
+
+    struct Piece *homeStraight[PLAYER_NO][MAX_HOME_STRAIGHT / PIECE_NO] = {NULL};
 }
