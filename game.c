fac8f8bde305c6b2b17b9b588d6dc143b7b600ab
mani-hash <manimehalan400@gmail.com>
2024-08-12 17:47:06 +0530

    added piece initializer

--- game.c
+++ game.c
@@ -4,3 +4,29 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+
+/* Initialization functions
+ */
+
+struct Piece createPiece(char namePrefix, char nameSuffix)
+{
+  char name[3] = {namePrefix, nameSuffix, '\0'};
+
+  struct Piece piece;
+
+  piece.cellNo = BASE;
+  piece.captured = 0;
+  strncpy(piece.name, name, sizeof(piece.name) - 1);
+  piece.clockWise = true;
+  piece.block = false;
+  piece.noOfApproachPasses = 0;
+
+  //define default effect status
+  piece.effect.effectActive = false;
+  piece.effect.pieceActive = true;
+  piece.effect.effectActiveRounds = 0;
+  piece.effect.diceMultiplier = 1;
+  piece.effect.diceDivider = 1;
+
+  return piece;
+}
